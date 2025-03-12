fa5ceab738136bc8eb7d05bcc98942d7f7bef908
Hesham Elsayed <heesham749@gmail.com>
2025-03-13 00:40:34 +0200

    add swagger configurations

--- MoviesApi/MoviesApi/Program.cs
+++ MoviesApi/MoviesApi/Program.cs
@@ -1,9 +1,11 @@
 
+using Microsoft.OpenApi.Models;
+
 namespace MoviesApi
 {
     public class Program
     {
-        public static void Main(string[] args) 
+        public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +14,41 @@ namespace MoviesApi
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(op =>
+            {
+                op.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "My Movies Api",
+                    Version = "v1",
+                    Contact = new OpenApiContact { Email = "HeshamElsayedAhmed@outlock.com", Name = "Me" }
+                });
+
+                op.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "Enter your token",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer",
+                });
+
+                op.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                       new OpenApiSecurityScheme
+                       {
+                           Reference = new OpenApiReference
+                           {
+                               Type = ReferenceType.SecurityScheme,
+                               Id = "Bearer"
+                           }
+                       },
+                       new List<string>()
+                    }
+
+                });
+            });
 
             var app = builder.Build();
 
